dcee4d883396d2527ef9d6801ffd3452986f4c67
Raesangur <pascalpinguin@gmail.com>
2021-09-06 18:10:30 -0400

    platbuilder

---
+++ MenuFact02/menufact/plats/builder/PlatSanteBuilder.java
@@ -0,0 +1,18 @@
+package menufact.plats.builder;
+
+import menufact.plats.PlatSante;
+
+public class PlatSanteBuilder extends PlatBuilder {
+    public PlatSanteBuilder() {
+        plat = new PlatSante();
+    }
+
+    public PlatSanteBuilder buildKCal(double kcal) {
+        ((PlatSante)plat).setKcal(kcal);
+        return this;
+    }
+    public PlatSanteBuilder buildChol(double chol) {
+        ((PlatSante)plat).setKcal(chol);
+        return this;
+    }
+}
