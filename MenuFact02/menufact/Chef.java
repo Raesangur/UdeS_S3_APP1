bb14a2443bd6a71b9fadd8abcf48ff9af7681faf
anthopop123 <72212667+anthopop123@users.noreply.github.com>
2021-09-08 12:50:56 -0400

    javadoc

--- MenuFact02/menufact/Chef.java
+++ MenuFact02/menufact/Chef.java
@@ -15,21 +15,40 @@ public class Chef {
     private Chef() {
     }
 
+    /**
+     * Permet de créer un chef ou de retourner le chef existant
+     *
+     * @return instance du chef
+     */
     public static synchronized Chef getInstance() {
         return instance == null ? instance = new Chef() : instance;
     }
 
+    /**
+     * donne le nom du chef
+     * @return nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * change le nom du chef
+     * @param nom string pour le nom du chef
+     */
     public void setNom(String nom) {
         if(nom != null){
             this.nom = nom;
         }
     }
 
-
+    /**
+     * Appeler par l'observer de la facture et permet de cuisiner le platChoisi en changeant ses états de préparations en consommant les ingrédients dans l'inventaire en appelant ses autres fonctions
+     * @param platACuisiner PlatChoisi qui va changer d'état et donner les ingrédients
+     * @return PlatChoisi un fois servi ou null
+     * @throws IngredientException S'il n'y a pas assez d'ingrédient pour le plat
+     * @throws PlatException Si le plat ne peut pas être cuisiner error avec un changement d'état
+     */
     public PlatChoisi cuisiner(PlatChoisi platACuisiner) throws IngredientException, PlatException {
         platACuisiner.setEtat(new Commande());
 
@@ -42,6 +61,13 @@ public class Chef {
         }
     }
 
+    /**
+     * Vérifie la présence des Ingrédients dans l'inventaire pour permet la préparation de la recette
+     * @param platAVerifier PlatChoisi qui donne les ingrédients et leur qty.
+     * @return boolean vrai si valable faux sinon
+     * @throws IngredientException return si les ingrédients ne sont pas possible
+     * @throws PlatException si on une qty négative
+     */
     private boolean verifierIngredient(PlatChoisi platAVerifier) throws IngredientException, PlatException {
         Inventaire inventaire = Inventaire.getInstance();
         Recette recette = platAVerifier.getPlat().getRecette();
@@ -62,6 +88,12 @@ public class Chef {
         return true;
     }
 
+    /**
+     * Changer l'état du plat choisi vers préparer et consommer les ingrédients
+     * @param platAPreparer PlatChoisi qui change d'état
+     * @throws IngredientException return si les ingrédients ne sont pas possible
+     * @throws PlatException Si le changement est imposible
+     */
     private void preparer(PlatChoisi platAPreparer) throws IngredientException, PlatException {
         platAPreparer.setEtat(new EnPreparation());
 
@@ -72,15 +104,30 @@ public class Chef {
         inventaire.consommerRecette(recette, platAPreparer.getQty(), platAPreparer.getPlat().getProportion());
     }
 
+    /**
+     * change l'état du platChoisi pour terminé
+     * @param platATerminer PlatChoisi qui change d'état
+     * @throws PlatException Si le changement d'état est impossible
+     */
     private void terminer(PlatChoisi platATerminer) throws PlatException{
         platATerminer.setEtat(new Termine());
     }
 
+    /**
+     * change l'état du platChoisi pour Servi
+     * @param platAServir PlatChoisi qui change d'état
+     * @return le plat à servir
+     * @throws PlatException Changement d'état impossible
+     */
     private PlatChoisi servir(PlatChoisi platAServir) throws PlatException {
         platAServir.setEtat(new Servi());
         return platAServir;
     }
 
+    /**
+     * Permet de représenter le chef sous forme de string
+     * @return String du chef
+     */
     @Override
     public String toString() {
         return "Chef: {\n\t" +
--- MenuFact02/menufact/Menu.java
+++ MenuFact02/menufact/Menu.java
@@ -11,38 +11,71 @@ public class Menu {
     private int courant;
     private ArrayList<PlatAuMenu> plat = new ArrayList<PlatAuMenu>();
 
+    /**
+     * Créer un menu contenant sa description
+     * @param description un string de description
+     */
     private Menu(String description) {
         this.description = description;
     }
 
+    /**
+     * refere ou créer un menu s'il existe déjà un menu
+     * @return instance du menu
+     */
     public synchronized static Menu getInstance() {
         return instance == null ? instance = new Menu(null) : instance;
     }
 
+    /**
+     * Donne la description du menu
+     * @return description du menu
+     */
     public String getDescription() {
         return description;
     }
+
+    /**
+     * Permet de Changer la description du menu
+     * @param description un string de description
+     */
     public void setDescription(String description) {
         if (description != null) {
             this.description = description;
         }
     }
 
+    /**
+     * Permet d'ajouter un Plat au menu sous forme de PlatAuMenu
+     * @param p un PlatAuMenu à ajouter
+     */
     public void ajoute(PlatAuMenu p)
     {
         plat.add(p);
     }
 
+    /**
+     * Permet de changer la position actuel du menu
+     * @param i un int représentant la position
+     */
     public void position(int i)
     {
         courant = i;
     }
 
+    /**
+     * donne le plat a la position courante du menu
+     * @return Plat courant
+     */
     public PlatAuMenu platCourant()
     {
         return plat.get(courant);
     }
 
+    /**
+     * Change la position vers la position suivante
+     * @throws MenuException si le courant+1 est plus grand que le size du plat on le return
+     */
     public void positionSuivante() throws MenuException
     {
         if (courant+1 >= plat.size())
@@ -51,6 +84,10 @@ public class Menu {
             courant++;
     }
 
+    /**
+     * Change la position courante pour la position précédente
+     * @throws MenuException si le courant-1 est plus petit que 0 on le return
+     */
     public void positionPrecedente() throws MenuException
     {
         if (courant-1 < 0)
@@ -59,6 +96,10 @@ public class Menu {
             courant--;
     }
 
+    /**
+     * Permet d'afficher le menu sous forme de String
+     * @return String du Menu
+     */
 
     @Override
     public String toString() {
